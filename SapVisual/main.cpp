af77f18f73e0d20fbf6f5dedb6ec3e5eb7921838
josue <josue.ccama@ucsp.edu.pe>
2015-08-06 18:16:22 -0500

    miu

--- SapVisual/main.cpp
+++ SapVisual/main.cpp
@@ -6,13 +6,19 @@
 #include "Repisas/repisamoneda.h"
 #include "Repisas/repisamarca.h"
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "Repisas/repisaimagenes.h"
 =======
+=======
+>>>>>>> origin/panochon
 #include "Repisas/repisavehiculotipo.h"
 #include "Repisas/repisaimagenes.h"
 #include "Repisas/repisavehiculorubro.h"
 #include "Repisas/repisaclientetipo.h"
 #include "Repisas/repisaestado.h"
+<<<<<<< HEAD
+>>>>>>> origin/panochon
+=======
 >>>>>>> origin/panochon
 int main(int argc, char *argv[])
 {
@@ -24,14 +30,20 @@ int main(int argc, char *argv[])
    //RepisaArticulo* w=new RepisaArticulo();
    //RepisaEmpleados* w=new RepisaEmpleados();
 <<<<<<< HEAD
+<<<<<<< HEAD
    RepisaMoneda* w=new RepisaMoneda();
   // RepisaImagenes* w=new RepisaImagenes();
 =======
+=======
+>>>>>>> origin/panochon
  //  RepisaVehiculoRubro* w=new RepisaVehiculoRubro();
  // RepisaImagenes* w=new RepisaImagenes();
 //    RepisaMoneda* w=new RepisaMoneda();
     // RepisaClienteTipo* w=new RepisaClienteTipo();
        RepisaEstado* w=new RepisaEstado();
+<<<<<<< HEAD
+>>>>>>> origin/panochon
+=======
 >>>>>>> origin/panochon
     w->show();
 
